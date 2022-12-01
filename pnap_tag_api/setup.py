c0a22c28d716bb37c2d4d8dd78b15f328ef00060
darrenccbill <93383166+darrenccbill@users.noreply.github.com>
2022-12-01 16:15:35 +0100

    Matrix strategy testing for multiple versions support (#23)
    matrix version testing for compatibility checks

--- pnap_tag_api/setup.py
+++ pnap_tag_api/setup.py
@@ -37,7 +37,7 @@ setup(
     author_email="support@phoenixnap.com",
     url="https://phoenixnap.com/bare-metal-cloud",
     keywords=["OpenAPI", "OpenAPI-Generator", "Tags API"],
-    python_requires=">=3.11",
+    python_requires=">=3.6",
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
