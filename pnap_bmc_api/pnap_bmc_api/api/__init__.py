84d2665302f2d8563816e8d76f28b0abce63bb54
sokoli <sokoli@ccbill.com>
2024-01-25 22:33:58 +0100

    manually clean up bmc api

--- pnap_bmc_api/pnap_bmc_api/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from pnap_bmc_api.api.quotas_api import QuotasApi
-from pnap_bmc_api.api.ssh_keys_api import SSHKeysApi
-from pnap_bmc_api.api.servers_api import ServersApi
-
