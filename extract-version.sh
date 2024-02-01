8265df80a819da1351dbec3985c518b7a705f145
sokoli <sokoli@ccbill.com>
2024-02-01 18:55:57 +0100

    add script

---
+++ extract-version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the regex pattern
+regex='VERSION = "(.*)"'
+
+# Read the contents of the file
+contents="$(cat $filename)"
+
+# Check if the contents match the regex
+if [[ $contents =~ $regex ]]; then
+    # Print the matched version
+    echo "${BASH_REMATCH[1]}"
+else
+    # Print an error message and exit with an error code
+    echo "Couldn't find a match for ($regex) in $filename."
+    exit 1
+fi
