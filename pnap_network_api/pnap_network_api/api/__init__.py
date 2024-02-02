a61a13bf20b828bf34034b042ba98b6c1df06e35
sokoli <sokoli@ccbill.com>
2024-02-02 14:33:28 +0100

    clean up network

--- pnap_network_api/pnap_network_api/api/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from pnap_network_api.api.private_networks_api import PrivateNetworksApi
-from pnap_network_api.api.public_networks_api import PublicNetworksApi
-
